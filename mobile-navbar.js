644be78811422c3afbd07379640853d886c8a050
eduardo329 <eduardo_rj_18@hotmail.comm>
2021-05-27 13:18:00 -0300

    Menu

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,13 +1,28 @@
 class MobileNavbar {
-    constructor(mobileMenu) {
+    constructor(mobileMenu, navList, navLinks) {
         this.mobileMenu = document.querySelector(mobileMenu);
         this.navList = document.querySelector(navList);
         this.navLinks = document.querySelector(navLinks);
         this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link) => {
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = 'navLinkFade 0.5s ease forwards 0.3s');
+        });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.animateLinks();
     }
 
     addClickEvent() {
-        this.mobileMenu.addEventListener("click");
+        this.mobileMenu.addEventListener("click", this.handleClick);
     }
 
     init() {
@@ -22,5 +37,7 @@ class MobileNavbar {
 const mobileNavbar = new MobileNavbar(
     ".mobile-menu",
     ".nav-list",
-    ".nav-list li",-
+    ".nav-list li",
 );
+
+mobileNavbar.init();
